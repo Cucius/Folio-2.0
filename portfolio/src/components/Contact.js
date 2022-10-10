ea65bff65ab4aaaf82f48edac9886a3b30294810
Cucius <Rylan19@Gmail.com>
2022-10-10 07:41:23 -0600

    Added helper for email validation/Added Contact form

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,15 +1,69 @@
+import React, { useState } from "react";
+
+// const Contact = () => {
+//   return (
+//     // <div className="flex-shrink-0 snap-start z-10 bg-lime-600 w-screen h-screen flex items-center justify-center text-8xl">Contact</div>;
+//     <div
+//       className="flex snap-start
+//                 items-center justify-center
+//                 bg-gradient-to-r from-moss to-veronese text-8xl
+//                 w-screen h-screen"
+//     ></div>
+//   );
+// };
+
+// import { validateEmail } from '../../utils/helpers';
+// if (!validateEmail(email) || !userName) {
+// setErrorMessage('Email or username is invalid');
+
+const FORM_ENDPOINT = "";
 const Contact = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
+  if (submitted) {
+    return (
+      <>
+        <div className="text-2xl">Thank you!</div>
+        <div className="text-md">We'll be in touch soon.</div>
+      </>
+    );
+  }
   return (
-    // <div className="flex-shrink-0 snap-start z-10 bg-lime-600 w-screen h-screen flex items-center justify-center text-8xl">Contact</div>;
     <div
       className="flex snap-start
-                items-center justify-center
-                bg-gradient-to-r from-moss to-veronese text-8xl
-                w-screen h-screen"
+    items-center justify-center
+    bg-gradient-to-r from-moss to-veronese text-8xl
+    w-screen h-screen"
     >
-      Contact Me
+      <div
+        className="h-screen flex flex-col 
+      items-center justify-center"
+      >
+        <div className="mb-3 pt-0">
+          <h3 className="text-center text-gray-400 text-s">Contact Us</h3>
+        </div>
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" action={FORM_ENDPOINT} onSubmit={handleSubmit} method="POST" target="_blank">
+          <div className="mb-3 pt-0">
+            <input type="text" placeholder="Your name" name="name" className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full" required />
+          </div>
+          <div className="mb-3 pt-0">
+            <input type="email" placeholder="Email" name="email" className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full" required />
+          </div>
+          <div className="mb-3 pt-0">
+            <textarea placeholder="Your message" name="message" className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full" required />
+          </div>
+          <div className="mb-3 pt-0">
+            <button className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="submit">
+              Send a message
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
-
 export default Contact;
