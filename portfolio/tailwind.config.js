1d240f1811581cb3ff0a428c361c2cce2a2f92b2
Cucius <Rylan19@Gmail.com>
2022-10-10 03:27:14 -0600

    Added/Fixed Tailwindcss

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,8 +1,36 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: [],
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        jonquil: "#f4d03f",
+        veronese: "#16a085",
+        moss: "#78924b",
+        salsa: "#f05d5e",
+        alice: "e7ecef",
+        gray: colors.neutral,
+        gray: {
+          900: "#12181b",
+          800: "#202225",
+          700: "#2a2e35",
+          600: "#454e56",
+          400: "#6c7983",
+          300: "#b2becd",
+          200: "#dbe1e8",
+          100: "#f8f8f8",
+        },
+      },
+    },
+    clipPath: {
+      // arrow: "polygon(0% 100%, 0% 0%, 90% 0%, 100% 50%, 90% 98%)",
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [require("tailwind-clip-path")],
 };
